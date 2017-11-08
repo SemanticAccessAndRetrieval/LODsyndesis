d6b43857463d97d99ec8bd45492e0f4ad51dae0c
mountanton <mountanton@csd.uoc.gr>
2017-11-08 12:23:07 +0200

    SAR License

--- src/gr/forth/ics/isl/sameAsCatalog/GetNeighborsSameAs.java
+++ src/gr/forth/ics/isl/sameAsCatalog/GetNeighborsSameAs.java
@@ -1,19 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+This code belongs to the Semantic Access and Retrieval (SAR) group of the 
+Information Systems Laboratory (ISL) of the 
+Institute of Computer Science (ICS) of the  
+Foundation for Research and Technology – Hellas (FORTH)
+
+Nobody is allowed to use, copy, distribute, or modify this work.
+It is published for reasons of research results reproducibility.
+
+© 2017, Semantic Access and Retrieval group, All rights reserved
  */
 package gr.forth.ics.isl.sameAsCatalog;
 
